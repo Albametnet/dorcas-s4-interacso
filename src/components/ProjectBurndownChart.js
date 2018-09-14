cb3a1c490c9d53238d1cb9d33bd81f37fa020084
andre <andreina.vargas@gmail.com>
2018-09-14 20:57:18 +0200

    fix correct colors

--- src/components/ProjectBurndownChart.js
+++ src/components/ProjectBurndownChart.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Chart } from 'devextreme-react';
 
-const date = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-const commits = [56, 56, 23, 5, 8, 9, 9, 67, 3, 3, 4, 4]
-const task = [58, 16, 13, 5, 8, 9, 9, 7, 13, 3, 4, 5]
+const date= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+const pending= [56, 56, 23, 5, 8, 9, 9, 67, 3, 3, 4, 4]
+const complete= [58, 16, 13, 5, 8, 9, 9, 7, 13, 3, 4, 5]
 
 const settings=
 {
@@ -11,11 +11,11 @@ const settings=
   argumentField: "date",
   color: "green"
 };
-const series=
+const series =
 [
   {
-    color: "#57718D",
-    valueField: "commits",
+    color: "#f5a623",
+    valueField: "pending",
     label: {
       visible: true,
       backgroundColor: "none",
@@ -29,8 +29,8 @@ const series=
   },
 
   {
-    color: "#98187D",
-    valueField: "task",
+    color: "#3dcef0",
+    valueField: "complete",
     label: {
       visible: true,
       backgroundColor: "none",
